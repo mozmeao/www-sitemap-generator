ed62535f1b77aa013b5561ed59678d43a88a0394
Paul McLanahan <pmac@mozilla.com>
2020-06-30 17:05:57 -0400

    Fix script and add after_script for GitLab

--- generate_sitemap_docker.sh
+++ generate_sitemap_docker.sh
@@ -5,11 +5,8 @@ set -exo pipefail
 docker-compose pull bedrock
 docker-compose build --pull generator
 docker-compose run --rm -u "$(id -u):$(id -g)" generator
-# store return code from this command so we can use it after we clean up
-RETCODE="$?"
-docker-compose down
 
-if [[ "$RETCODE" == "0" && "$1" == "commit" ]]; then
+if [[ "$1" == "commit" ]]; then
     if git status --porcelain | grep -E "\.(json|xml)\$"; then
         git add sitemap.json etags.json sitemaps
         git commit -m "Update sitemaps data"
@@ -23,5 +20,3 @@ fi
 if [[ -n "$SNITCH_URL" ]]; then
     curl "$SNITCH_URL"
 fi
-
-exit $RETCODE
