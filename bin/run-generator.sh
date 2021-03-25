f54116105ec0832fa7d1a49344a42cdc41b397e1
Paul McLanahan <pmac@mozilla.com>
2021-03-25 11:22:50 -0400

    Fix generator failure to start

--- bin/run-generator.sh
+++ bin/run-generator.sh
@@ -4,6 +4,8 @@ bin/run-db-download.py
 python manage.py l10n_update
 python manage.py update_sitemaps
 sed -i -E -e 's|<html class="windows x86 no-js".+>|<html class="windows x86 no-js">|' bedrock/base/templates/base-protocol.html
+# needed for app to start
+touch data/last-run-update_locales data/last-run-download_database
 bin/run-prod.sh > /dev/null &
 urlwait http://localhost:8000 60
 sleep 2
